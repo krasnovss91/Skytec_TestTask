c47be2ff1cdb5606115d23773be45c86b603fe52
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-18 18:13:31 +0400

    synchronized

--- src/com/company/controller/ClanController.java
+++ src/com/company/controller/ClanController.java
@@ -5,7 +5,7 @@ import com.company.service.ClanManager;
 
 public class ClanController {
 
-    public void incGold(long clanId, int gold){//этот метод может вызываться до 1000 раз разными потоками
+    public synchronized void incGold(long clanId, int gold){//этот метод может вызываться до 1000 раз разными потоками
         Clan clan = ClanManager.getClan(clanId);
         int newGold = gold + clan.getGold();//добавлена возможность увеличения золота
         clan.setGold(newGold);
@@ -14,7 +14,7 @@ public class ClanController {
         ClanManager.saveClan(clanId);
     }
 
-   public void decGold(long clanId, int gold){//метод для уменьшения количества золота
+   public synchronized void decGold(long clanId, int gold){//метод для уменьшения количества золота
       Clan clan = ClanManager.getClan(clanId);
       int newGold = clan.getGold() - gold;
       clan.setGold(newGold);
