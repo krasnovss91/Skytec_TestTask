29d43bbc0cf36d9e50fde6216345bef9c4c1dde1
krasnovss91 <krasnovstanislav91@gmail.com>
2022-08-18 16:54:25 +0400

    incGold

--- src/com/company/controller/ClanController.java
+++ src/com/company/controller/ClanController.java
@@ -7,6 +7,11 @@ public class ClanController {
 
     public void incGold(long clanId, int gold){//этот метод может вызываться до 1000 раз разными потоками
         Clan clan = ClanManager.getClan(clanId);
+        int newGold = gold + clan.getGold();//добавлена возможность увеличения золота
+        clan.setGold(newGold);
+
         clan.incGold(gold);
     }
+
+
 }
